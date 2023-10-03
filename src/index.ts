2de5e71eaa535c47933417dc3a04e989c5f0bca5
Neale Taylor <nealeptaylor@gmail.com>
2023-10-03 15:27:57 -0400

    working with functions

--- src/index.ts
+++ src/index.ts
@@ -11,16 +11,29 @@
 // numbers.forEach(n => n.)
 
 // Tuples- fixed number array. Passing too many variables will result in type error. Good for key value pairs.
-let user: [number, string] = [1, 'Neale']
+// let user: [number, string] = [1, 'Neale']
 
-// Enums- list of related constants. if you define enum as const enum it will produce more refined js code in dist folder
-// const small = 1;
-// const medium = 2;
-// const large = 3;
-// becomes
-enum Size { Small = 1, Medium, Large };
-// Small is set to 0 by default, Medium to 1, Large to 2. Can be specifically set ex. { Small = 1, Medium = 2, Large = 3}
-let mySize: Size = Size.Medium;
-console.log(mySize) // 2
+// // Enums- list of related constants. if you define enum as const enum it will produce more refined js code in dist folder
+// // const small = 1;
+// // const medium = 2;
+// // const large = 3;
+// // becomes
+// enum Size { Small = 1, Medium, Large };
+// // Small is set to 0 by default, Medium to 1, Large to 2. Can be specifically set ex. { Small = 1, Medium = 2, Large = 3}
+// let mySize: Size = Size.Medium;
+// console.log(mySize) // 2
 
 // Functions
+// You can specify the return type after vars in function declaration
+// TS will tell you if variables are unused- noImplicitReturns and noUnusedLocals in config
+// Add ? to variable in function declaration to make it optional. You can also provide a default value with = to make it have a value in case one isn't provided when calling the function.
+// function calculateTax(income: number, taxYear = 2022): number {
+//   if (taxYear < 2022) {
+//     return income * 1.2;
+//   }
+//   return income * 1.3;
+// }
+
+// calculateTax(10_000, 2022)
+
+// Objects
