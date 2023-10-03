d88f7d9846ad033408b4bd960203aab64e6f9386
Neale Taylor <nealeptaylor@gmail.com>
2023-10-03 15:20:22 -0400

    learning different declarations

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,12 @@
 "use strict";
-console.log("hello world");
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let user = [1, 'Neale'];
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+;
+let mySize = Size.Medium;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-console.log("hello world");
+// let sales = 123_456_789;
+// let course = 'TypeScript';
+// let is_published = true;
+// let level;
 
-let age: number = 20;
+// function render(document) {
+//   console.log(document)
+// }
 
-if (age < 50) 
-  age += 10;
-console.log(age);
+// let numbers: number[] = [];
+// numbers.forEach(n => n.)
+
+// Tuples- fixed number array. Passing too many variables will result in type error. Good for key value pairs.
+let user: [number, string] = [1, 'Neale']
+
+// Enums- list of related constants. if you define enum as const enum it will produce more refined js code in dist folder
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+// becomes
+enum Size { Small = 1, Medium, Large };
+// Small is set to 0 by default, Medium to 1, Large to 2. Can be specifically set ex. { Small = 1, Medium = 2, Large = 3}
+let mySize: Size = Size.Medium;
+console.log(mySize) // 2
+
+// Functions
