52c979a55b6707a8bb7a07ed210e562476310b95
Neale Taylor <nealeptaylor@gmail.com>
2023-10-05 14:23:49 -0400

    playing with variables

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css';
 
+let name: string
+let age: number;
+let isStudent: boolean;
+let hobbies: string[];
+let role:[number, string];
+
+
 function App() {
+
   return (
     <div className="App">
-
+      Hello World
     </div>
   );
 }
