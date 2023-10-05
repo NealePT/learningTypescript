bab8306d5b2dda9e4324f1dd42837efc63ffcfcd
Neale Taylor <nealeptaylor@gmail.com>
2023-10-05 14:35:41 -0400

    changing function

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -5,11 +5,30 @@ let age: number | string;
 let isStudent: boolean;
 let hobbies: string[];
 let role:[number, string];
-let printName: (name:string) => void;
+// Return void returns undefined, return never returns nothing
+let printName: (name:string) => never;
 
-// type Person = {
-//   name:string,
-//   age?:number,
+interface Person {
+  name:string,
+  age?:number,
+}
+interface Guy extends Person {
+  profession: string;
+}
+
+// type X = {
+//   a:string;
+//   b:number;
+// }
+// type Y = X & {
+//   c:string;
+//   d:number;
+// }
+// let y: Y = {
+//   a: "iofnas",
+//   b: 16,
+//   c: "fias",
+//   d: 15,
 // }
 
 // let person: Person = {
@@ -18,7 +37,9 @@ let printName: (name:string) => void;
 
 // let lostOfPeople: Person[];
 
-function App() {
+// let personName: unknown; 
+
+const App = () => {
 
   return (
     <div className="App">
