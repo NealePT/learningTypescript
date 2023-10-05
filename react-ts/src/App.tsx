d2bdea16edc96ed8cdc52c62be2595f88ee1c348
Neale Taylor <nealeptaylor@gmail.com>
2023-10-05 14:24:58 -0400

    creating custom types

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -6,12 +6,22 @@ let isStudent: boolean;
 let hobbies: string[];
 let role:[number, string];
 
+type Person = {
+  name:string,
+  age:number,
+}
+
+let person: Person = {
+  name: "Neale",
+  age: 26
+}
 
 function App() {
 
   return (
     <div className="App">
       Hello World
+      {person.name}
     </div>
   );
 }
