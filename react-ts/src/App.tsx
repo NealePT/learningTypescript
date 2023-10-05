8014450f9a587b808e1f1dc6cf799eea8012649d
Neale Taylor <nealeptaylor@gmail.com>
2023-10-05 14:35:59 -0400

    remove test code

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,44 +1,5 @@
 import './App.css';
 
-let name: string;
-let age: number | string;
-let isStudent: boolean;
-let hobbies: string[];
-let role:[number, string];
-// Return void returns undefined, return never returns nothing
-let printName: (name:string) => never;
-
-interface Person {
-  name:string,
-  age?:number,
-}
-interface Guy extends Person {
-  profession: string;
-}
-
-// type X = {
-//   a:string;
-//   b:number;
-// }
-// type Y = X & {
-//   c:string;
-//   d:number;
-// }
-// let y: Y = {
-//   a: "iofnas",
-//   b: 16,
-//   c: "fias",
-//   d: 15,
-// }
-
-// let person: Person = {
-//   name: "Neale",
-// }
-
-// let lostOfPeople: Person[];
-
-// let personName: unknown; 
-
 const App = () => {
 
   return (
