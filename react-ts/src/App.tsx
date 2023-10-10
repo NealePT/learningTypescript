e6c8431c480602270dd6e1d6ac76a871b9cffe6c
Neale Taylor <nealeptaylor@gmail.com>
2023-10-10 15:07:35 -0400

    fix type bug:

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import './components/styles.css'
 import InputField from './components/InputField';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Todo } from './model';
 import TodoList from './components/TodoList';
 
@@ -22,7 +22,10 @@ const App: React.FC = () => {
   <div className="App">
     <span className="heading">Taskify</span>
     <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-    <TodoList todos={todos} setTodos={setTodos} />
+    {todos.map((t) => (
+      <li>{t.todo}</li>
+    ))}
+    <TodoList todos={todos} setTodos={setTodos}/>
   </div>
   );
 }
--- react-ts/src/components/ToDoList.tsx
+++ react-ts/src/components/ToDoList.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { Todo } from "../model";
-import "./styles.css";
+import './styles.css';
+import {Todo} from '../model'
 
-interface Props{
+interface Props {
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoList: React.FC = ({todos, setTodos}:Props) => {
-  return <div className="todos">{}</div>
-};
+const TodoList:React.FC<Props> = ( {todos, setTodos} :Props) => {
+  return (
+    <div className="todos">
+      hi
+    </div>
+  )
+}
 
 export default TodoList
