004f6c0a8ddde4a60dea1aeba1bc135bb2a2a24e
Neale Taylor <nealeptaylor@gmail.com>
2023-10-10 15:27:44 -0400

    add styling

--- react-ts/src/components/SingleTodo.tsx
+++ react-ts/src/components/SingleTodo.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Todo } from "../model"
+import {AiFillEdit, AiFillDelete} from 'react-icons/ai'
+import {MdDone} from 'react-icons/md'
+import './styles.css'
 
 type Props = {
   todo: Todo,
@@ -11,9 +14,13 @@ type Props = {
 const SingleTodo = ({todo, todos, setTodos}: Props) => {
   return (
     <form className="todos__single">
-      <span className="todos__single--text">
-        {todo.todo}
-      </span>
+      <span className="todos__single--text">{todo.todo}</span>
+
+      <div>
+        <span className="icon"><AiFillEdit/></span>
+        <span className="icon"><AiFillDelete/></span>
+        <span className="icon"><MdDone/></span>
+      </div>
     </form>
   )
 }
