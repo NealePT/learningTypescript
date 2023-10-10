266dd6d79e0ab7e7158bad652ac48638160b26d3
Neale Taylor <nealeptaylor@gmail.com>
2023-10-10 15:19:59 -0400

    building list functionality

--- react-ts/src/components/ToDoList.tsx
+++ react-ts/src/components/ToDoList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './styles.css';
 import {Todo} from '../model'
+import SingleTodo from './SingleTodo'
 
 interface Props {
   todos: Todo[];
@@ -10,7 +11,9 @@ interface Props {
 const TodoList:React.FC<Props> = ( {todos, setTodos} :Props) => {
   return (
     <div className="todos">
-      hi
+      {todos.map((todo) => (
+        <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
+      ))}
     </div>
   )
 }
